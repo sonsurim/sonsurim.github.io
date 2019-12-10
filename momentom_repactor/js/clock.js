4eaa9bed7a62cba896581d6e5e315be2d7c9837c
sonsurim <surim014@naver.com>
2019-12-10 21:11:01 +0900

    momentom repactor

--- momentom_repactor/js/clock.js
+++ momentom_repactor/js/clock.js
@@ -1,4 +1,4 @@
-const clockContainer = document.querySelector(".js-clock");
+const clockContainer = document.querySelector(".clock-container");
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime(){
@@ -10,11 +10,11 @@ function timeSet(date){
     const hours = date.getHours();
     const minutes = date.getMinutes();
 
-    if(hours < 12){ //오전인 경우 (아침~낮)
+    if(hours < 12){
         clockTitle.innerText = `${hours}:${minutes < 10? `0${minutes}`:minutes}`;
-    }else if(hours === 12){ //자정인 경우 (밤 12시)
+    }else if(hours === 12){
         clockTitle.innerText = `${hours}:${minutes < 10? `0${minutes}`:minutes}`;
-    }else{ //오후인 경우 (낮~밤)
+    }else{
         let currentHours = hours;
         currentHours = hours - 12;
         clockTitle.innerText = `${currentHours}:${minutes < 10 ? `0${minutes}`:minutes}`;
