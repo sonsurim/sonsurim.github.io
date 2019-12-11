6ad92e99768d80045f932c6bec16ad1188a2a425
sonsurim <surim014@naver.com>
2019-12-11 17:31:54 +0900

    momentom repactor

---
+++ test/todo.js
@@ -0,0 +1,26 @@
+const todo = document.querySelector('form');
+const input = todo.querySelector('input');
+
+const arr = [];
+
+function saveToDo(){
+    localStorage.setItem("todo",JSON.stringify(arr));
+}
+
+function showTodo(){
+    const newObj = {
+        id : arr.length + 1,
+        text : input.value,
+        complete : false
+    };
+    arr.push(newObj);
+    saveToDo();
+}
+
+function submitHandler(e){
+    e.preventDefault();
+    showTodo(input.value);
+    input.value = "";
+}
+
+todo.addEventListener('submit', submitHandler);
