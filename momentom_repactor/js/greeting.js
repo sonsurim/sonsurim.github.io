3b25fec39503f03ed359be0f4272e6fba0013d66
sonsurim <surim014@naver.com>
2019-12-16 19:28:48 +0900

    todolist repactor

--- momentom_repactor/js/greeting.js
+++ momentom_repactor/js/greeting.js
@@ -1,102 +1,39 @@
-const greetingFirst = document.querySelector(".js-greeting-1");
-const greetingForm = document.querySelector(".js-greeting-form");
-const greetingInput = greetingForm.querySelector("input");
-const greeting = document.querySelector(".js-greeting");
+const greetingContainer = document.querySelector('.greeting-container');
+const greetingTitle = greetingContainer.querySelector('p');
+const greetingForm = greetingContainer.querySelector('form');
+const greetingInput = greetingContainer.querySelector('input');
 
-const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
-
-// ect
-const greetingToDo = document.querySelector(".js-todo");
-const greetingToDoForm = document.querySelector(".js-todo-form");
-const greetingBody = document.querySelector("body");
-
-const greetingClock = document.querySelector(".js-clock");
-const greetingWeather = document.querySelector(".js-weather")
-const greetingPlace = document.querySelector(".js-place")
-// ect
-
-// etc function
-function greetingTime(){
-    const date = new Date();
-    const hours = date.getHours();
-    const currentUser = localStorage.getItem(USER_LS);
-
-    if(hours < 12){ // 오전인 경우
-        greeting.innerText = `Good Morning, ${currentUser}.`;
-    }else if(hours === 12){ //오후 12시인 경우
-        greeting.innerText = `Good AfterNoon, ${currentUser}.`;
-    }else{
-        greeting.innerText = `Good AfterNoon, ${currentUser}.`;
-    }
-}
-
-function greetingToDoPaint(){
-    greetingToDo.classList.add(SHOWING_CN);
-    greetingToDoForm.classList.add(SHOWING_CN);
-    const toDoScript = document.createElement("script");
-    toDoScript.src = "todo.js";
-    greetingBody.appendChild(toDoScript);
-}
-
-function greetingClockPaint(){
-    greetingClock.classList.add(SHOWING_CN);
-    const clockScript = document.createElement("script");
-    clockScript.src = "clock.js";
-    greetingBody.appendChild(clockScript);
-}
-
-function greetingWeatherPaint(){
-    greetingWeather.classList.add(SHOWING_CN);
-    greetingPlace.classList.add(SHOWING_CN);
-}
-// etc function
-
-function saveName(text){
-    localStorage.setItem(USER_LS,text);
-}
-
-
-function paintName(text){
-    greetingForm.classList.remove(SHOWING_CN);
-    greetingFirst.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    saveName(text);
-    greetingTime(Text);
-    setInterval(greetingTime, 1000);
-}
-
-function submitHandler(event){
-    event.preventDefault();
-    const currentValue = greetingInput.value;
-    paintName(currentValue);
-    saveName(currentValue);
-    greetingToDoPaint();
-    greetingClockPaint();
-    greetingWeatherPaint();
-}
-
-function askForName(){
-    greetingForm.classList.add(SHOWING_CN);
-    greetingFirst.classList.add(SHOWING_CN);
-    greetingForm.addEventListener("submit",submitHandler);
-}
+let currentUser;
 
 function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
-        askForName();
-    }else{
-        paintName(currentUser);
-        greetingToDoPaint();
-        greetingClockPaint();
-        greetingWeatherPaint();
-    }
+    currentUser = localStorage.getItem('user');
+    if (currentUser !== null){
+        showName();
+        loadTodo();
+    };
+};
 
-}
+function showName(){
+    greetingContainer.classList.add('hide');
+    saveName();
+};
+
+function saveName(){
+    localStorage.setItem('user', currentUser);
+};
+
+function greetingSubmitHandler(e){
+    currentUser = greetingInput.value;
+    greetingInput.value = '';
+    showName();
+};
 
 function init(){
+    const clear = document.querySelector('.clear');
     loadName();
-}
+    greetingForm.addEventListener('submit', greetingSubmitHandler);
+    todoForm.addEventListener('submit', todoSubmitHandler);
+    clear.addEventListener('click', clickClearHandler);
+};
 
 init();
--- momentom_repactor/js/todo.js
+++ momentom_repactor/js/todo.js
@@ -1,70 +1,85 @@
-const toDoForm = document.querySelector(".js-todo-form");
-const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector(".js-todo-list");
+const todoContainer = document.querySelector('.todo-container');
+const todoTitle = todoContainer.querySelector('p');
+const todoForm = todoContainer.querySelector('form');
+const todoInput = todoContainer.querySelector('input');
 
-const TODOS_LS = "ToDoList";
-let toDos = [];
+let todoList = [];
 
-function saveToDo(){
-    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
-}
-
-
-function clickDelHandler(event){
-    const btn = event.target;
-    const li = btn.parentNode;
-    toDoList.removeChild(li);
-    const cleanToDos = toDos.filter(function(toDo){
-        return toDo.id !== parseInt(li.id);
-    });
-    toDos = cleanToDos;
-    saveToDo();
-}
-
-function paintToDo(text){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const checkBtn = document.createElement("button");
-    const buttonSpan = document.createElement("span");
-    const newID = toDos.length + 1;
-    checkBtn.classList.add("checkBtn");
-    checkBtn.addEventListener("click",clickDelHandler);
-    span.innerText = text;
-    li.id = newID;
-    li.classList.add("todo-list");
-    li.appendChild(checkBtn);
-    li.appendChild(span);
-    checkBtn.appendChild(buttonSpan);
-    toDoList.appendChild(li);
-    const toDosObj = {
-        text : text,
-        id : newID
-    }
-    toDos.push(toDosObj);
-    saveToDo();
-}
-
-
-function loadToDo(){
-    const loadedToDo = localStorage.getItem(TODOS_LS);
-    if(loadedToDo !== null){
-        const parsedToDo = JSON.parse(loadedToDo);
-        parsedToDo.forEach(function(toDo){
-            paintToDo(toDo.text);
+function loadTodo(){
+    todoContainer.classList.remove('hide');
+    const currentTodo = localStorage.getItem('todolist');
+    if(currentTodo !== null){
+        const parseTodo = JSON.parse(currentTodo);
+        parseTodo.forEach(function(toDo){
+            showTodo(toDo.text);
         });
     }
+};
+
+function showTodo(text){
+    todoContainer.classList.remove('hide');
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const completeBtn = document.createElement('button');
+    const delBtn = document.createElement('button');
+    completeBtn.addEventListener("click",clickCompleteHandler);
+    completeBtn.classList.add('complete-btn');
+    delBtn.addEventListener("click",clickDelHandler);
+    delBtn.classList.add('delete-btn');
+    li.dataset.id = todoList.length + 1;
+    span.textContent = text;
+    delBtn.textContent = `X`;
+    li.appendChild(completeBtn);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+    const newObj = {
+        id : todoList.length + 1,
+        text : text,
+        complete : false
+    };
+    todoList.push(newObj);
+    saveTodo();
+};
+
+function saveTodo(){
+    localStorage.setItem('todolist',JSON.stringify(todoList));
+};
+
+function todoSubmitHandler(e){
+    e.preventDefault();
+    showTodo(todoInput.value);
+    todoInput.value = '';
 }
 
-function submitHandler(event){
-    event.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-    toDoInput.value = "";
+function clickCompleteHandler(e){
+    const li = e.target.parentNode;
+    const currentTodo = localStorage.getItem('todolist');
+    const parseTodo = JSON.parse(currentTodo);
+    parseTodo[li.dataset.id - 1].complete = !(parseTodo[li.dataset.id - 1].complete);
+    todoList = parseTodo;
+    saveTodo();
 }
 
-function init(){
-    loadToDo();
-    toDoForm.addEventListener("submit",submitHandler);
+function clickDelHandler(e){
+    const ul = document.querySelector('ul');
+    const li = e.target.parentNode;
+    ul.removeChild(li);
+    const cleanToDos = todoList.filter(function(toDo){
+        // toDo에는 객체들이 각각 담겨있음 그 객체들중 갖고있는 id랑 내가 클릭한 li id랑 같으면 삭제, 다르면 그냥 함수 종료
+        return toDo.id !== parseInt(li.dataset.id);
+    });
+    todoList = cleanToDos;
+    saveTodo();
 }
 
-init();
+function clickClearHandler(e){
+    const ul = document.querySelector('ul');
+    while(ul.hasChildNodes()){
+        ul.removeChild(ul.firstChild);
+    }
+    localStorage.removeItem('todolist');
+    todoList = [];
+    saveTodo();
+}
