c2d2e728bafcfcc5ed0431e6ec97998e8180510b
sonsurim <surim014@naver.com>
2019-12-17 19:26:23 +0900

    디자인 점검

--- momentom_repactor/js/todo.js
+++ momentom_repactor/js/todo.js
@@ -4,6 +4,7 @@ const todoForm = todoContainer.querySelector('form');
 const todoInput = todoContainer.querySelector('input');
 
 let todoList = [];
+let count = 0;
 
 function loadTodo(){
     todoContainer.classList.remove('hide');
@@ -12,14 +13,14 @@ function loadTodo(){
     todoTitle.textContent = `${currentUser}님의 오늘의 할 일`;
     if(currentTodo !== null){
         const parseTodo = JSON.parse(currentTodo);
+        todoList = parseTodo;
         parseTodo.forEach(function(toDo){
             showTodo(toDo.text);
         });
     }
 };
 
-function showTodo(text){
-    todoContainer.classList.remove('hide');
+function makeTodo(text){
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
     const span = document.createElement('span');
@@ -31,7 +32,6 @@ function showTodo(text){
     delBtn.classList.add('delete-btn');
     li.dataset.id = todoList.length + 1;
     span.textContent = text;
-    completeBtn.textContent = `✍`;
     delBtn.textContent = `X`;
     li.appendChild(completeBtn);
     li.appendChild(span);
@@ -44,24 +44,83 @@ function showTodo(text){
     };
     todoList.push(newObj);
     saveTodo();
+
+}
+
+function showTodo(text){
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const completeBtn = document.createElement('button');
+    const delBtn = document.createElement('button');
+    completeBtn.addEventListener("click",clickCompleteHandler);
+    completeBtn.classList.add('complete-btn');
+    delBtn.addEventListener("click",clickDelHandler);
+    delBtn.classList.add('delete-btn');
+    span.textContent = text;
+    delBtn.textContent = `X`;
+    li.appendChild(completeBtn);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+    getId();
+    completeCheck();
+    saveTodo();
+};
+
+function completeCheck(){
+    const li = document.querySelectorAll('li');
+    const btn = li[count].querySelector('button');
+    if(todoList[count].complete){
+        if(todoList[count].complete){
+            li[count].classList.add('complete');
+            btn.textContent = '✍';
+        }
+    }
+    count++;
+}
+
+function deleteTodo(){
+    const ul = document.querySelector('ul');
+    while(ul.hasChildNodes()){
+        ul.removeChild(ul.firstChild);
+    }
+    localStorage.removeItem('todolist');
+    todoList = [];
 };
 
 function saveTodo(){
     localStorage.setItem('todolist',JSON.stringify(todoList));
 };
 
+function getId(){
+    const li = document.querySelectorAll('li');
+    for(let i = 0; i < li.length; i++){
+        li[i].dataset.id = i + 1;
+        todoList[i].id = parseInt(li[i].dataset.id);
+    }
+    saveTodo();
+}
+
+
+// event handler
 function todoSubmitHandler(e){
     e.preventDefault();
-    showTodo(todoInput.value);
+    makeTodo(todoInput.value);
     todoInput.value = '';
 }
 
 function clickCompleteHandler(e){
     const li = e.target.parentNode;
-    const currentTodo = localStorage.getItem('todolist');
-    const parseTodo = JSON.parse(currentTodo);
-    parseTodo[li.dataset.id - 1].complete = !(parseTodo[li.dataset.id - 1].complete);
-    todoList = parseTodo;
+    if(!(todoList[li.dataset.id - 1].complete)){
+        todoList[li.dataset.id - 1].complete =!(todoList[li.dataset.id - 1].complete);
+        li.classList.add('complete');
+        e.target.textContent = '✍';
+    }else if(todoList[li.dataset.id - 1].complete){
+        todoList[li.dataset.id - 1].complete =!(todoList[li.dataset.id - 1].complete);
+        li.classList.remove('complete');
+        e.target.textContent = '';
+    }
     saveTodo();
 }
 
@@ -69,32 +128,21 @@ function clickDelHandler(e){
     const ul = document.querySelector('ul');
     const li = e.target.parentNode;
     ul.removeChild(li);
-    const cleanToDos = todoList.filter(function(toDo){
-        // toDo에는 객체들이 각각 담겨있음 그 객체들중 갖고있는 id랑 내가 클릭한 li id랑 같으면 삭제, 다르면 그냥 함수 종료
+    const cleanTodo = todoList.filter(function(toDo){
         return toDo.id !== parseInt(li.dataset.id);
     });
-    todoList = cleanToDos;
+    todoList = cleanTodo;
     saveTodo();
 }
 
 function clickClearHandler(e){
-    const ul = document.querySelector('ul');
-    while(ul.hasChildNodes()){
-        ul.removeChild(ul.firstChild);
-    }
-    localStorage.removeItem('todolist');
-    todoList = [];
+    deleteTodo();
     saveTodo();
 }
 
 function clickResetHandler(){
-    const ul = document.querySelector('ul');
-    while(ul.hasChildNodes()){
-        ul.removeChild(ul.firstChild);
-    }
     localStorage.removeItem('user');
-    localStorage.removeItem('todolist');
-    todoList = [];
+    deleteTodo();
     saveTodo();
     window.location.reload();
 }
