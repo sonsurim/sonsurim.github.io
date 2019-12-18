01572fde32bbe08a4f5f747ddba64e64a3284600
sonsurim <surim014@naver.com>
2019-12-18 14:38:29 +0900

    투두리스트 타이틀 변경

--- paintingBoard/app.js
+++ paintingBoard/app.js
@@ -8,10 +8,10 @@ const saveBtn = document.getElementById("js-save");
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 500;
 
-ctx.fillStyle = "white"; 
+ctx.fillStyle = "white";
 ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
-ctx.fillStyle = INITIAL_COLOR; 
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; //그 선의 너비가 2.5px
 
 let painting = false;
@@ -20,10 +20,10 @@ let filling = false;
 function mousemoveHandler(event){ //canvas에서 마우스를 움직일때 발생
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){ //클릭하지 않고 마우스를 움직였을 때에는 
+    if(!painting){ //클릭하지 않고 마우스를 움직였을 때에는
         ctx.beginPath(); //그림이 들어갈 새로운 경로를 만든다
         ctx.moveTo(x,y); // 마우스의 x,y 좌표로 그 경로를 옮긴다. → 내가 마우스를 움직이는 모든 순간에 경로를 만든다(아직 path를 사용하지 않은 상태)
-       
+
     }else{
         ctx.lineTo(x,y);
         ctx.stroke();
