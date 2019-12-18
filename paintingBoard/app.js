9f0d67233ddc206b92dcc81a8749c815be85455f
sonsurim <surim014@naver.com>
2019-12-18 21:08:34 +0900

    페인팅보드 init 함수 추가 + 리팩토리

--- paintingBoard/app.js
+++ paintingBoard/app.js
@@ -1,28 +1,34 @@
-const canvas = document.getElementById("js-canvas");
-const ctx = canvas.getContext("2d");
-const colors = document.getElementsByClassName("js-color");
-const range = document.getElementById("js-range");
-const mode = document.getElementById("js-mode");
-const saveBtn = document.getElementById("js-save");
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+const colorContainer = document.querySelector('.control-colors');
+const colors = document.querySelectorAll('.colors');
+const range = document.querySelector('input');
+const mode = document.querySelector('.fill');
+const saveBtn = document.querySelector('.save');
 
-const INITIAL_COLOR = "#2c2c2c";
+const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 500;
 
-ctx.fillStyle = "white";
+ctx.fillStyle = 'white';
 ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
-ctx.lineWidth = 2.5; //그 선의 너비가 2.5px
+//그 선의 너비가 2.5px
+ctx.lineWidth = 2.5;
 
 let painting = false;
 let filling = false;
 
-function mousemoveHandler(event){ //canvas에서 마우스를 움직일때 발생
+//canvas에서 마우스를 움직일때 발생
+function mousemoveHandler(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){ //클릭하지 않고 마우스를 움직였을 때에는
-        ctx.beginPath(); //그림이 들어갈 새로운 경로를 만든다
-        ctx.moveTo(x,y); // 마우스의 x,y 좌표로 그 경로를 옮긴다. → 내가 마우스를 움직이는 모든 순간에 경로를 만든다(아직 path를 사용하지 않은 상태)
+    //클릭하지 않고 마우스를 움직였을 때에는
+    if(!painting){
+        //그림이 들어갈 새로운 경로를 만든다
+        ctx.beginPath();
+        // 마우스의 x,y 좌표로 그 경로를 옮긴다. → 내가 마우스를 움직이는 모든 순간에 경로를 만든다(아직 path를 사용하지 않은 상태)
+        ctx.moveTo(x,y);
 
     }else{
         ctx.lineTo(x,y);
@@ -39,8 +45,10 @@ function stopPainting(){
 }
 
 
-function colorClickHandler(event){
-    const color = event.target.style.backgroundColor;
+function colorClickHandler(e){
+    // css로 가져오게끔 변경해야됨
+    const color = e.target.style.backgroundColor;
+    console.log(e.target);
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
 }
@@ -79,26 +87,27 @@ function saveClickHandler(){
     link.click();
 }
 
+function init(){
+    if(canvas) {
+        canvas.addEventListener("mousemove",mousemoveHandler);
+        canvas.addEventListener("mousedown",startPainting);
+        canvas.addEventListener("mouseleave",stopPainting);
+        canvas.addEventListener("mouseup",stopPainting);
+        canvas.addEventListener("click",canvasClickHandler);
+        canvas.addEventListener("contextmenu",rightClickHandler);
+    }
+    if(range){
+        range.addEventListener("input", rangeChangeHandler);
+    }
+    if(mode){
+        mode.addEventListener("click", modeClickHandler);
+    }
+    if(saveBtn){
+        saveBtn.addEventListener("click", saveClickHandler);
+    }
 
-if(canvas) {
-    canvas.addEventListener("mousemove",mousemoveHandler);
-    canvas.addEventListener("mousedown",startPainting);
-    canvas.addEventListener("mouseleave",stopPainting);
-    canvas.addEventListener("mouseup",stopPainting);
-    canvas.addEventListener("click",canvasClickHandler);
-    canvas.addEventListener("contextmenu",rightClickHandler);
+    // Array.from(colors).forEach(color => color.addEventListener("click",colorClickHandler));
+    colorContainer.addEventListener('click', colorClickHandler);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click",colorClickHandler));
-
-if(range){
-    range.addEventListener("input", rangeChangeHandler);
-}
-
-if(mode){
-    mode.addEventListener("click", modeClickHandler);
-}
-
-if(saveBtn){
-    saveBtn.addEventListener("click", saveClickHandler);
-}
+init();
