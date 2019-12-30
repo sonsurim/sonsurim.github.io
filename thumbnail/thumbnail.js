3fe49c56dcea65c01730b9e19393ff6c17e56644
sonsurim <surim014@naver.com>
2019-12-31 00:37:40 +0900

    thumbnail repactor

--- thumbnail/thumbnail.js
+++ thumbnail/thumbnail.js
@@ -1,3 +1,4 @@
+(function(){
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const textForm = document.querySelector('form');
@@ -7,104 +8,85 @@ const inputCheck = document.querySelector('.checkbox');
 const inputText = textForm.querySelector('input');
 const selectElem = document.querySelector('select');
 const defaultPalette = document.querySelector('.default-palette');
-const colorPalette = document.querySelector('.color-palette');
+const paletteModal = document.querySelector('.palette-modal');
 const clearBtn = document.querySelector('.clear');
 const downloadBtn = document.querySelector('.download');
 
 const canvasObj = {
+    visible : false,
+    checked : true,
     width : '800',
     height : '300',
     bgColor : '#2d98da',
-    textColor : '#fff',
     fontSize : 40,
-    checked : true,
-    visible : false
+    fontColor : '#fff',
 };
-
 
 function setBgColor(){
     ctx.fillStyle = canvasObj.bgColor;
     ctx.fillRect(0,0,canvasObj.width,canvasObj.height);
     setText();
-}
-
-
+};
 function setSize(){
     canvas.width = canvasObj.width;
     canvas.height = canvasObj.height;
     setBgColor();
-}
-
+};
 function setText(){
     ctx.font = `${canvasObj.fontSize}px Arial`;
-    ctx.fillStyle = canvasObj.textColor;
+    ctx.fillStyle = canvasObj.fontColor;
     ctx.textBaseline = 'middle';
     ctx.textAlign = "center";
     ctx.fillText(inputText.value, canvasObj.width/2, canvasObj.height/2);
-}
+};
 
-function setClear(){
+/* event Handler */
+function setClearHandler(){
     canvasObj.bgColor = '#2d98da';
     ctx.fillStyle = canvasObj.bgColor;
     ctx.fillRect(0,0,canvasObj.width,canvasObj.height);
     inputText.value ="";
-}
-
+};
 function defaultPaletteHandler(e){
     const defaultColors = document.querySelectorAll('.default-colors');
-    if(e.target !== defaultColors[9]){
+
+    if(e.target !== defaultColors[9]){  // ect color는 따로 배경색을 지정해두지 않아서
         if(canvasObj.checked === true){
             defaultColors.forEach(elem => {
                 if(e.target === elem){
                     canvasObj.bgColor = elem.style.backgroundColor;
                 }
             });
-
             setBgColor();
-
         }else if(canvasObj.checked !== true){
             defaultColors.forEach(elem => {
                 if(e.target === elem){
-                    canvasObj.textColor = elem.style.backgroundColor;
+                    canvasObj.fontColor = elem.style.backgroundColor;
                 }
             });
-
             setBgColor();
-            setText();
-        }
-    }
-}
-
-function colorPaletteHandler(e){
+        };
+    };
+};
+function paletteModalHandler(e){
     const colors = document.querySelectorAll('.colors');
 
     if(canvasObj.checked === true){
         colors.forEach(elem => {
             if(e.target === elem){
                 canvasObj.bgColor = elem.style.backgroundColor;
-            }
+            };
         });
-
         setBgColor();
-
     }else if(canvasObj.checked !== true){
         colors.forEach(elem => {
             if(e.target === elem){
-                canvasObj.textColor = elem.style.backgroundColor;
-            }
+                canvasObj.fontColor = elem.style.backgroundColor;
+            };
         });
-
-        setBgColor();
-        setText();
-    }
-}
-
-function fontSizeHandler(e){
-    canvasObj.fontSize = e.target.value;
-    setBgColor();
-    setText();
-}
-
+        setBgColor();   // 기존 텍스트 지우는 역할
+    };
+};
 function switchHandler(e){
     const font = document.querySelector('.icon-fontsize');
     const brush = document.querySelector('.icon-brush');
@@ -119,15 +101,8 @@ function switchHandler(e){
         inputCheck.classList.remove('checked');
         brush.classList.remove('showing');
         font.classList.add('showing');
-    }
-}
-
-function submitHandler(e){
-    e.preventDefault();
-    setBgColor();
-    setText();
-}
-
+    };
+};
 function downloadHandler(){
     const image = canvas.toDataURL();
     const link = document.createElement("a");
@@ -136,9 +111,32 @@ function downloadHandler(){
     link.execCommand('SaveAs');
     link.download = "thumbnail";
     link.click();
-}
+};
+function popModalHandler(e){
+    const defaultColors = document.querySelectorAll('.default-colors');
+
+    if(e.target === defaultColors[9]){
+        canvasObj.visible = !canvasObj.visible;
+
+        if(canvasObj.visible){
+            paletteModal.classList.add('showing');
+        }else if(!canvasObj.visible){
+            paletteModal.classList.remove('showing');
+        }
+    }else{
+        canvasObj.visible = false;
+        paletteModal.classList.remove('showing');
+    }
+};
 
 function init(){
+    document.addEventListener('click', popModalHandler);
+    defaultPalette.addEventListener('click', defaultPaletteHandler);
+    paletteModal.addEventListener('click', paletteModalHandler);
+    inputCheck.addEventListener('click', switchHandler);
+    clearBtn.addEventListener('click', setClearHandler);
+    downloadBtn.addEventListener('click', downloadHandler);
+
     inputWidth.addEventListener('keyup', function(){
         canvasObj.width = this.value;
         setSize();
@@ -147,34 +145,17 @@ function init(){
         canvasObj.height = this.value;
         setSize();
     });
-    textForm.addEventListener('submit', submitHandler);
-    selectElem.addEventListener('change', fontSizeHandler);
-    clearBtn.addEventListener('click', setClear);
-    downloadBtn.addEventListener('click', downloadHandler);
-
-    document.addEventListener('click', function(e){
-        const defaultColors = document.querySelectorAll('.default-colors');
-
-        if(e.target === defaultColors[9]){
-            canvasObj.visible = !canvasObj.visible;
-
-            if(canvasObj.visible){
-                colorPalette.classList.add('showing');
-            }else if(!canvasObj.visible){
-                colorPalette.classList.remove('showing');
-            }
-        }else{
-            canvasObj.visible = false;
-            colorPalette.classList.remove('showing');
-        }
+    textForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        setBgColor();
+    });
+    selectElem.addEventListener('change', function(e){
+        canvasObj.fontSize = e.target.value;
+        setBgColor();
     });
 
-    defaultPalette.addEventListener('click', defaultPaletteHandler);
-    colorPalette.addEventListener('click', colorPaletteHandler);
-    inputCheck.addEventListener('click', switchHandler);
-
     setSize();
-    setBgColor();
 };
 
 init();
+})();
